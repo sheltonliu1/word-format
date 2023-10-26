62a7db5abdb234c12e674596f3d2bfbb973a5d45
sheltonliu <sheltonliu@tencent.com>
2023-10-26 20:30:38 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,74 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+const data = [
+  {
+    unitNo: 1,
+    items: [
+      {
+        no: 1,
+        word: 'abandon',
+        isEmphasizeWord: true,
+        sense: [
+          {
+            type: 'vt',
+            text: '放弃；遗弃；抛弃',
+            isEmphasize: false
+          }
+        ],
+        extraSense: [
+          {
+            word: 'abandoned dogs',
+            type: 'adj',
+            text: '被遗弃的狗'
+          }
+        ],
+        phrase: []
+      },
+      {
+        no: 2,
+        word: 'ability',
+        sense: [
+          {
+            type: 'n',
+            text: '能力，才能'
+          }
+        ],
+        extraSense: [
+          {
+            word: 'able',
+            type: 'a',
+            text: '能够，有能力的'
+          },
+          {
+            word: 'unable',
+            type: 'adj',
+            text: '不能的'
+          },
+          {
+            word: 'diabled',
+            type: 'adj',
+            text: '残疾的'
+          },
+          {
+            word: 'disability',
+            type: 'n',
+            text: '残疾'
+          }
+        ],
+        phrase: [
+          {
+            text: '使某人能够做某事',
+            phrase: 'enable sb to do sth'
+          }
+        ]
+      }
+    ]
+  }
+]
+</script>
+
+<template>
+  <main>
+    {{data}}
+  </main>
+</template>
